6172e7f1ecf43d9b5b7b740a6a9716a50ab120e6
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2023-05-05 00:43:29 +0900

    add chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,31 @@
-import type { NextPage } from 'next';
-import type { AppType, AppProps } from 'next/app';
-import type { ReactElement, ReactNode } from 'react';
-import { DefaultLayout } from '~/components/DefaultLayout';
-import { trpc } from '~/utils/trpc';
+import type { NextPage } from 'next'
+import type { AppProps, AppType } from 'next/app'
+import type { ReactElement, ReactNode } from 'react'
+import { DefaultLayout } from '~/components/DefaultLayout'
+import { trpc } from '~/utils/trpc'
+import { theme } from '~/pages/theme'
+import { SaasProvider } from '@saas-ui/react'
 
 export type NextPageWithLayout<
   TProps = Record<string, unknown>,
   TInitialProps = TProps,
 > = NextPage<TProps, TInitialProps> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
+  getLayout?: (page: ReactElement) => ReactNode
+}
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
+  Component: NextPageWithLayout
+}
 
 const MyApp = (({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout =
-    Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>);
+    Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>)
 
-  return getLayout(<Component {...pageProps} />);
-}) as AppType;
+  return (
+    <SaasProvider theme={theme}>
+      {getLayout(<Component {...pageProps} />)}
+    </SaasProvider>
+  )
+}) as AppType
 
-export default trpc.withTRPC(MyApp);
+export default trpc.withTRPC(MyApp)
